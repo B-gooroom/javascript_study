7b891127422e389a312bedf55b3dd25610ad27b4
sean <j.sean1316@gmail.com>
2021-05-07 20:00:29 +0900

    firebase(Read)

--- firebase/test_1/public/members.js
+++ firebase/test_1/public/members.js
@@ -8,3 +8,25 @@ const membersCreate = function () {
     membersRead();
   });
 };
+
+const membersRead = function () {
+  axios.get('https://be-gooroom-default-rtdb.firebaseio.com/members.json').then(function (response) {
+    console.log('Done membersRead', response.data);
+    const members = response.data;
+    const tbody = document.getElementById('tbody-members');
+    tbody.innerHTML = '';
+    let index = 0;
+    for (let key in members) {
+      const member = members[key];
+      const tr = document.getElementById('tr-template-members').cloneNode(true);
+      tbody.appendChild(tr);
+      document.getElementsByName('members-name')[index].innerHTML = member.name;
+      document.getElementsByName('members-age')[index].value = member.age;
+      document.getElementsByName('members-key')[index].value = key;
+      document.getElementsByName('members-update')[index].index = index;
+      document.getElementsByName('members-delete')[index].index = index;
+      index++;
+    }
+  });
+};
+membersRead();
